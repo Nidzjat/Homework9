ff09daaf6c9a5be0d04bc187c085e2ba41a6a3e0
Nidjat <dotalol202@mail.ru>
2023-07-14 06:53:39 +0300

    Задача 2

---
+++ task66/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine("Введите D:");
+int d = int.Parse(ReadLine()!);
+WriteLine("Введите C:");
+int c = int.Parse(ReadLine()!);
+
+WriteLine(PrintNumbers(d, c));
+int PrintNumbers(int d, int c)
+{
+    if (c == d || c == 1)
+    {
+        return 1;
+    }
+    int s = PrintNumbers(d, c - 1) + c;
+    return s;
+}
